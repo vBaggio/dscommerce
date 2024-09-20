2074720ae38d6a27b4715d18e0bd9412487cbf60
vbaggio <vnc.baggio@gmail.com>
2024-09-20 11:40:52 -0300

    created and mapped entity: Payment

--- src/main/java/com/vbaggio/dscommerce/entities/Payment.java
+++ src/main/java/com/vbaggio/dscommerce/entities/Payment.java
@@ -2,43 +2,37 @@ package com.vbaggio.dscommerce.entities;
 
 import java.time.Instant;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private OrderStatus status;
 
-	@ManyToOne
-	@JoinColumn(name = "client_id")
-	private User client;
-	
-	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-	private Payment payment;
+	@OneToOne
+	@MapsId
+	private Order order;
 
-	public Order() {
+	public Payment() {
 	}
 
-	public Order(Long id, Instant moment, OrderStatus status, User client) {
+	public Payment(Long id, Instant moment, Order order) {
 		super();
 		Id = id;
 		this.moment = moment;
-		this.status = status;
-		this.client = client;
+		this.order = order;
 	}
 
 	public Long getId() {
@@ -57,20 +51,12 @@ public class Order {
 		this.moment = moment;
 	}
 
-	public OrderStatus getStatus() {
-		return status;
+	public Order getOrder() {
+		return order;
 	}
 
-	public void setStatus(OrderStatus status) {
-		this.status = status;
-	}
-
-	public User getClient() {
-		return client;
-	}
-
-	public void setClient(User client) {
-		this.client = client;
+	public void setOrder(Order order) {
+		this.order = order;
 	}
 
 }
