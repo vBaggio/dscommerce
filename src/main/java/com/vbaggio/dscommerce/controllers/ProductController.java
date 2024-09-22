f53c19920d3abd944e6911211d62b8a40e776b34
vbaggio <vnc.baggio@gmail.com>
2024-09-22 01:03:28 -0300

    created controller: Product; did some route tests

---
+++ src/main/java/com/vbaggio/dscommerce/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.vbaggio.dscommerce.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vbaggio.dscommerce.entities.Product;
+import com.vbaggio.dscommerce.repositories.ProductRepository;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductRepository repository;
+
+	@GetMapping
+	public String teste() {
+		Optional<Product> result = repository.findById(1L);
+
+		Product product = result.get();
+
+		return product.getName();
+	}
+}
